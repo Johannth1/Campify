85aaacabdd843f5ba1165078c024618bd18b34ba
Johann Næss <venn.johann@hotmail.com>
2020-02-18 22:52:54 +0100

    Fixed login screen + style

--- TDS200_h19kont_5002/src/app/login/login.page.ts
+++ TDS200_h19kont_5002/src/app/login/login.page.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
 import { AuthService } from '../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -17,36 +16,32 @@ export class LoginPage implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router,
-    private toastController: ToastController
+    private router: Router
   ) { }
 
+  // Vi bruker async for å bruke "Promises" på en tryggere måte, siden vi ikke vet når brukeren skal skrive inn e-posten og passordet. 
+  // Login funksjon som tar i bruk auth.service.ts classen, som tar inn en e-post og
+  // passord og sender de inn til firebase, hvis disse blir godkjent navigere den videre til home page
   async loginUser() {
     try {
-      this.authService.loginUser({ email: this.user.email, password: this.user.password });
-      this.router.navigate(['home']);
-    } catch (e) {
-      const toast = await this.toastController.create({
-        message: e.message,
-        duration: 2000
-      });
-      toast.present();
-      console.warn(e);
+      this.authService.loginUser({
+          email: this.user.email,
+          password: this.user.password });
+        this.router.navigate(['home']);
+      } catch(error) {
+        console.log = error.message;
     }
+  } 
 
-  }
-
+  // Se kommentaren over..
   async registerUser() {
     try {
-      this.authService.registerUser({ email: this.user.email, password: this.user.password });
-      this.router.navigate(['home']);
-    } catch (e) {
-      const toast = await this.toastController.create({
-        message: e.message,
-        duration: 2000
-      });
-      toast.present();
-      console.warn(e);
+      this.authService.registerUser({ 
+          email: this.user.email, 
+          password: this.user.password });
+        this.router.navigate(['home']);
+      } catch(error) {
+        console.log = error.message;
     }
   }
 
